68278977580d2819d903bbee0957cc66c39e2b4e
sjaakluthart <mail@sjaakluthart.nl>
2014-04-09 09:48:03 +0200

    site map and swipe event added

--- eindopdracht/js/script.js
+++ eindopdracht/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 
-    $('header img:nth-of-type(2)').on('click', function(){
+    $('header img:nth-of-type(2)').click(function(){
         $('nav').slideToggle(450);
         });
+
+      $("nav").swipe( {
+        //Generic swipe handler for all directions
+        swipeUp:function(event, direction, distance, duration, fingerCount) {
+          $('nav').slideToggle(450);
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold:0
+      });
 
     $('fieldset:nth-of-type(2) label:nth-of-type(1), fieldset:nth-of-type(2) input:nth-of-type(1)').click(function(){
       
